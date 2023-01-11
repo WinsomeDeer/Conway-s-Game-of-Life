e42ca1783516744b8cdb7e444ed8c71562c294b1
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2023-01-11 23:37:47 +0000

    Update Langtons_ant.cpp

--- Langtons_ant.cpp
+++ Langtons_ant.cpp
@@ -1,1 +1,20 @@
+#include <time.h>
+#include <algorithm>
 
+class Langtons_Ant{
+  public:
+    void print(char[] A){
+      const int n = sizeof(A)/sizeof(A[0]);
+      for(int i = 0; i < n; i++){
+          for(int j = 0; j < n; j++){
+                cout << A[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+  
+  return 0;
+}
