0517da495327000cf963d1dfad770505a4df5106
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2023-04-16 22:04:30 +0100

    Update GOL.pyx

--- src./GOL.pyx
+++ src./GOL.pyx
@@ -1,35 +1,47 @@
 import numpy as np
+cimport numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 cimport cython
-import time
+from libc.stdlib cimport rand, srand, RAND_MAX
+from libc.time cimport time
+import time as T
 # Game of Life
+srand(time(NULL))
 # Function to generate random grid.
-def random_grid(N):
-    res = np.random.choice([0,1], N*N, p = [0.6,0.4]).reshape(N, N)
-    cdef int[:,:] res_view = res
+cdef random_grid(int N):
+    cdef np.ndarray res = np.zeros((N,N), dtype = np.intc)
+    cdef int[:, ::1] res_view = res
+    cdef int i, j
+    for i in range(N):
+        for j in range(N):
+            if rand() < 0.6*(RAND_MAX + 1):
+                res_view[i, j] = 1
+            else:
+                res_view[i, j] = 0
     return res_view
 # C function to calculate the neighbours total.
-@cython.boundscheck(False)
-@cython.wraparound(False)
-cdef int neigh(int[:,::1] grid, int i, int j, int N):
-    cdef int total = 0, ii, dx, dy
-    cdef list dr = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
-    for ii in range(8):
-        dx = i + dr[ii][0]
-        dy = j + dr[ii][1]
-        if dx >= 0 and dx < N and dy >= 0 and dy < N:
-            total += grid[dx][dy]
+cdef int neigh(int[:, ::1] grid, int i, int j, int N):
+    cdef int total = 0, ii, jj, dx, dy
+    for ii in range(-1,2):
+        for jj in range(-1,2):
+            if ii == 0 and jj == 0:
+                continue
+            else:
+                dx = i + ii
+                dy = j + jj
+                if dx >= 0 and dx < N and dy >= 0 and dy < N:
+                    total += grid[dx, dy]
     return total
 # Function to calculate the next iteration.
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def run(int frame_num, img, int[:, ::1] grid, int N):
     cdef int i, j, total
-    tmp = np.zeros(shape = (N, N), dtype = np.intc)
-    cdef int[:, :] tmp_view = tmp
+    cdef np.ndarray tmp = np.zeros(shape = (N, N), dtype = np.intc)
+    cdef int[:, ::1] tmp_view = tmp
     cdef int[:, ::1] grid_view = grid
-    start = time.time()
+    start = T.time()
     for i in range(N):
         for j in range(N):
             total = neigh(grid_view, i, j, N)
@@ -42,14 +54,13 @@ def run(int frame_num, img, int[:, ::1] grid, int N):
                 if total == 3:
                     tmp_view[i, j] = 1
     img.set_data(tmp_view)
-    grid_view[:, :] = tmp_view[:,:]
-    end = time.time()
+    grid_view[:, :] = tmp_view[:, :]
+    end = T.time()
     print(end - start)
     return img
 # Animation function.
 def main():
-    cdef int N
-    N = 100
+    cdef int N = 1000
     grid = random_grid(N)
     fig, ax = plt.subplots()
     ax.set_yticklabels([])
@@ -57,6 +68,5 @@ def main():
     img = ax.imshow(grid, interpolation = 'nearest')
     ani = animation.FuncAnimation(fig, run, fargs=(img, grid, N, ), frames = 10, interval = 50, save_count = 50)
     plt.show()
-
 if __name__ == '__main__':
     main()
